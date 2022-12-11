637f994a12ceb46b9267933a2eee163a8754c595
igobb <t.golab06@gmail.com>
2022-12-11 20:37:31 +0100

    Ready service and connection to Sanity / Ready folder structure

---
+++ schemas/informations.js
@@ -0,0 +1,22 @@
+export default {
+    name:'informations',
+        title:'Informations',
+    type: 'document',
+    fields:[
+    {
+        name:'title',
+        title:'Title',
+        type:'string'
+    },
+    {
+        name:'description',
+        title:'Description',
+        type:'string'
+    },
+    {
+        name:'link',
+        title:'Link',
+        type: 'string'
+    },
+]
+}
